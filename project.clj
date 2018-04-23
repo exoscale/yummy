f5351830c60d292c4a9f6aea38d923ba46b35bfe
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-04-23 17:01:44 +0200

    artifact-group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/yummy "0.2.2"
+(defproject exoscale/yummy "0.2.2"
   :description       "YAML configuration for Clojure application"
   :url               "https://github.com/exoscale/yummy"
   :license           {:name "MIT License"
