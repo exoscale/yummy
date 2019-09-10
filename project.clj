a773153efe847f10744bcb300b1f352e5ff1bc6e
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-09-10 17:20:24 +0200

    Version 0.2.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/yummy "0.2.8"
+(defproject exoscale/yummy "0.2.9-SNAPSHOT"
   :description       "YAML configuration for Clojure application"
   :url               "https://github.com/exoscale/yummy"
   :license           {:name "MIT License"
