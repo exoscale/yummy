12d587dd9783647333d0946e4a084952096f7c29
Max Penet <max.penet@exoscale.ch>
2020-09-22 12:45:04 +0200

    Version 0.2.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/yummy "0.2.10"
+(defproject exoscale/yummy "0.2.11-SNAPSHOT"
   :description       "YAML configuration for Clojure application"
   :url               "https://github.com/exoscale/yummy"
   :license           {:name "MIT License"
