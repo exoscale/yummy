8f06b7534b0b61b1611dd9d857201073d1447042
Vitaly Samigullin <vrs@pilosus.org>
2023-07-05 11:51:40 +0200

    Bump outdated deps; fix snakeyaml classes constructors

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject exoscale/yummy "0.2.12-SNAPSHOT"
+(defproject exoscale/yummy "0.2.13-SNAPSHOT"
   :description       "YAML configuration for Clojure application"
   :url               "https://github.com/exoscale/yummy"
   :license           {:name "MIT License"
                       :url  "https://github.com/exoscale/yummy/tree/master/LICENSE"}
-  :plugins           [[lein-codox   "0.10.2"]
-                      [lein-ancient "0.6.15"]]
+  :plugins           [[lein-codox   "0.10.8"]
+                      [lein-ancient "0.7.0"]]
   :codox             {:source-uri  "https://github.com/exoscale/yummy/blob/{version}/{filepath}#L{line}"
                       :output-path "target/docs"
                       :metadata    {:doc/format :markdown}}
   :javac-options     ["-target" "1.8" "-source" "1.8"]
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
-  :dependencies      [[org.clojure/clojure "1.10.1"]
-                      [org.yaml/snakeyaml  "1.25"]
-                      [exoscale/cloak "0.1.1"]
-                      [expound             "0.7.2"]]
+  :dependencies      [[org.clojure/clojure "1.11.1"]
+                      [org.yaml/snakeyaml  "2.0"]
+                      [exoscale/cloak "0.1.10"]
+                      [expound             "0.9.0"]]
   :profiles          {:dev {:resource-paths ["test/resources"]}}
   :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]]
   :global-vars       {*warn-on-reflection* true}
